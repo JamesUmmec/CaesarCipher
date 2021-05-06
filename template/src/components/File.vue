4d01edb1f8a54e69abe0c3e81ee5c5b7a60334a9
JamesUmmex <dreamingboat@outlook.com>
2021-05-06 12:46:58 +0800

    File tag

---
+++ template/src/components/File.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="tag">
+		<div id="icon"><img src=""></div>
+		<div id="info">
+			<div id="name">{{name}}</div>
+			<div id="path">{{path}}</div>
+			<div id="size">{{size}}</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: "File",
+	data: function() {
+		return {
+			name: "",
+			path: "",
+			size: ""
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
